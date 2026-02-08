416e5516268bd399bf5ac89d25840f8f87e4df07
furka <furkajr@mail.ru>
2026-02-08 08:18:14 +0400

    0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pigeon-bot",
-    version="0.1.0",
+    version="0.1.1",
     author="BenimFurka",
     description="A Python library for interacting with Pigeon Messenger",
     long_description=long_description,
